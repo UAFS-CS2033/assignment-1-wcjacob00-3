2cf616d953267f56fd070ac963b776009f72640a
wcjacob00 <146022173+wcjacob00@users.noreply.github.com>
2024-09-19 10:51:03 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -23,16 +23,50 @@ public class Server{
         while(buffer != null && buffer.length() != 0){
             System.out.println(buffer);
             buffer = in.readLine();
+            if(buffer.contains("GET"){
+                request("docroot/home.html" , "text/html", out);
+            }
         }
 
-        out.printf("HTTP/1.1 200 OK\n");
-        out.printf("Content-Length: 34\n");
-        out.printf("Content-Type: text/html\n\n");
+        //out.printf("HTTP/1.1 200 OK\n");
+        //out.printf("Content-Length: 34\n");
+        //out.printf("Content-Type: text/html\n\n");
        
-        out.printf("<h1>Welcome to web server</h1>");
+        //out.printf("<h1>Welcome to web server</h1>");
         in.close();
         out.close();
     }
+
+     void request(String filePath , String contentType , PrintWriter out) throws IOException{
+        File file = new File(filePath);
+        int content = (int) file.length();
+
+        out.printf("HTTP/1.1 200 OK\n");
+        out.printf("Content-Length: %d\n", contentLength);
+        out.printf("Content-Type: %s\n\n", contentType);
+
+        System.out.printf("HTTP/1.1 200 OK\n");
+        System.out.printf("Content-Length: %d\n", contentLength);
+        System.out.printf("Content-Type: %s\n\n", contentType);
+
+        if(contentType.equals("image/png")){
+            OutputStream out = (clientSocket.getOutputStream());
+            FileInputStream in = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while((bytesRead = in.read(buffer)) != -1){
+                out.write(buffer, 0 , bytesRead);
+           }
+        } else {
+            BufferedReader br = new BufferedReader(new FileReader (file));
+            String buffer = br.readLine();
+            while(buffer != null){
+                out.println(buffer);
+                buffer = br.readLine();
+            } 
+        }
+    }
+
 
     public void run() throws IOException{
         boolean running = true;
